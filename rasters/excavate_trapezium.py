687fd272943c81845eb8723bb3daaf1ba9ede3a7
Siebe Bosch <siebe@hydroconsult.nl>
2023-07-04 09:31:55 +0200

    'gitignor'

--- rasters/excavate_trapezium.py
+++ rasters/excavate_trapezium.py
@@ -2,26 +2,8 @@ import rasterio
 import geopandas as gpd
 import numpy as np
 from rasterio.features import geometry_mask
-from shapely.geometry import Point, LineString
-
-# Replace with your file paths
-elevation_tiff = 'C:\\SYNC\\PROJECTEN\\H3115.SIBELCO.Pilots\\02.Devon\\01.GIS\\UK_ND_Lidar_2022_DTM_20221230_05_interpolated.tif'
-channel_shapefile = 'C:\\SYNC\\PROJECTEN\\H3115.SIBELCO.Pilots\\02.Devon\\03.ChannelBuilder\\input\\Rivers_subselection_refined.shp'
-output_tiff = 'C:\\SYNC\\PROJECTEN\\H3115.SIBELCO.Pilots\\02.Devon\\03.ChannelBuilder\\output\\excavated_elevation.tif'
-
-# Read GeoTIFF and polyline shapefile
-with rasterio.open(elevation_tiff) as elevation_src:
-    elevation = elevation_src.read(1)
-    elevation_transform = elevation_src.transform
-
-channels = gpd.read_file(channel_shapefile)
-
-# Define a function to interpolate trapezium properties at a specific point
-def interpolate_properties(dist, channel):
-    bed_level = channel['BEDLEVELUP'] + dist * (channel['BEDLEVELDN'] - channel['BEDLEVELUP'])
-    bed_width = channel['BEDWIDTHUP'] + dist * (channel['BEDWIDTHDN'] - channel['BEDWIDTHUP'])
-    slope = channel['SLOPEUP'] + dist * (channel['SLOPEDN'] - channel['SLOPEUP'])
-    return bed_level, bed_width, slope
+from shapely.geometry import Point, LineString, box
+from rtree import index
 
 def nearest_channel_segment(point, linestring):
     min_dist = float('inf')
@@ -35,52 +17,91 @@ def nearest_channel_segment(point, linestring):
             min_dist = dist
             nearest_segment = segment
             nearest_fraction = segment.project(point) / segment.length
+        print(f"Segment: {segment}, Distance: {dist}, Min Distance: {min_dist}")
 
     return nearest_segment, nearest_fraction
 
-# Excavate the elevation grid
+
+elevation_tiff = 'C:\\SYNC\\PROJECTEN\\H3115.SIBELCO.Pilots\\02.Devon\\01.GIS\\UK_ND_Lidar_2022_DTM_20221230_05_interpolated.tif'
+channel_shapefile = 'C:\\SYNC\\PROJECTEN\\H3115.SIBELCO.Pilots\\02.Devon\\03.ChannelBuilder\\input\\Rivers_subselection_refined.shp'
+output_tiff = 'C:\\SYNC\\PROJECTEN\\H3115.SIBELCO.Pilots\\02.Devon\\01.GIS\\UK_ND_Lidar_2022_DTM_20221230_05_interpolated_excavated.tif'
+maxdepth = 3
+
+with rasterio.open(elevation_tiff) as elevation_src:
+    elevation = elevation_src.read(1)
+    elevation_transform = elevation_src.transform
+
+channels = gpd.read_file(channel_shapefile)
+
+idx = index.Index()
+
+mask = np.zeros(elevation.shape, dtype=bool)
 for _, channel in channels.iterrows():
     linestring = channel['geometry']
-    max_buf_distance = channel['BEDWIDTHUP'] / 2 + 5 * channel['SLOPEUP']
+    max_buf_distance = channel['BEDWIDTHUP'] / 2 + maxdepth * channel['SLOPEUP']
     buffer = linestring.buffer(max_buf_distance)
+    masked = geometry_mask([buffer], transform=elevation_transform, out_shape=elevation.shape, invert=True)
+    mask |= masked
 
-    # Convert the buffer to a mask
-    mask = geometry_mask([buffer], transform=elevation_transform, out_shape=elevation.shape, invert=True)
+    cell_id = 0
+    for row, col in np.argwhere(masked):
+        x, y = elevation_src.xy(row, col)
+        idx.insert(cell_id, (x, y, x, y), (row, col))
+        cell_id += 1
 
-    # Iterate through the grid cells within the buffered area
-    for row, col in np.argwhere(mask):
+total_channels = len(channels)
+
+for channel_idx, (_, channel) in enumerate(channels.iterrows(), start=1):
+    print(f"Processing channel {channel_idx} of {total_channels}")
+
+    linestring = channel['geometry']
+    max_buf_distance = channel['BEDWIDTHUP'] / 2 + maxdepth * channel['SLOPEUP']
+    buffer = linestring.buffer(max_buf_distance)
+    bounds = buffer.bounds
+
+    for item in idx.intersection(bounds, objects=True):
+        x_min, y_min, x_max, y_max = item.bbox
+        row, col = item.object
         x, y = elevation_src.xy(row, col)
         point = Point(x, y)
+        
+        # Rest of the code
 
-        # Get the nearest channel segment and the distance along it
         nearest_segment, nearest_fraction = nearest_channel_segment(point, linestring)
+        bed_level_up = channel['BEDLEVELUP']
+        bed_level_dn = channel['BEDLEVELDN']
+        bed_width_up = channel['BEDWIDTHUP']
+        bed_width_dn = channel['BEDWIDTHDN']
+        slope_up = channel['SLOPEUP']
+        slope_dn = channel['SLOPEDN']
 
-        # Interpolate the trapezium properties at the nearest point on the channel
-        dist = nearest_segment.length * nearest_fraction
-        bed_level, bed_width, slope = interpolate_properties(dist, channel)
 
-        # Calculate the perpendicular distance from the point to the channel
-        perp_dist = point.distance(nearest_segment)
+        bed_level = bed_level_up + nearest_fraction * (bed_level_dn - bed_level_up)
+        bed_width = bed_width_up + nearest_fraction * (bed_width_dn - bed_width_up)
+       
+        slope = slope_up + nearest_fraction * (slope_dn - slope_up)
 
-        # Excavate the grid cell if it is within the trapezium area
-        if perp_dist <= bed_width / 2:
-            horizontal_dist = bed_width / 2 - perp_dist
-            vertical_dist = horizontal_dist / slope
-            depth = min(vertical_dist, 5)  # Limit depth to 5 m
+        dist_from_channel = point.distance(nearest_segment)
 
-            new_elevation = bed_level - depth
-            if new_elevation < elevation[row, col]:
-                elevation[row, col] = new_elevation
+        if dist_from_channel <= bed_width / 2:
+            z_min = bed_level - dist_from_channel / slope
+            z_max = bed_level + dist_from_channel / slope
+            z = elevation[row, col]
 
-# Save the modified elevation grid to a new GeoTIFF file
+            if z_min <= z <= z_max:
+                elevation[row, col] = max(z_min, z - maxdepth)
+    break
+
+# Save the modified elevation grid to a new GeoTIFF
+
 with rasterio.open(
     output_tiff,
     'w',
     driver='GTiff',
-    height=elevation_src.height,
-    width=elevation_src.width,
+    height=elevation.shape[0],
+    width=elevation.shape[1],
     count=1,
-    dtype=elevation_src.dtypes[0],
+    dtype=elevation.dtype,
     crs=elevation_src.crs,
     transform=elevation_transform,
 ) as dst:
